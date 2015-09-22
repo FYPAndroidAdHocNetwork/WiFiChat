0a85b3b1cb9af2e840f3fd671046dd90d653f4bc
Qilin Wang <wangqilin129@gmail.com>
2015-09-22 13:32:12 +0800

    added Jansen's code, will work based on it

---
+++ app/src/main/java/com/colorcloud/wifichat/ChatActivity.java
@@ -0,0 +1,203 @@
+package com.colorcloud.wifichat;
+
+import static com.colorcloud.wifichat.Constant.MSG_PUSHOUT_DATA;
+import static com.colorcloud.wifichat.Constant.MSG_REGISTER_ACTIVITY;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.colorcloud.wifichat.ChatFragment.MsgRow;
+import com.colorcloud.wifichat.R;
+
+public class ChatActivity extends Activity {
+	
+	public static final String TAG = "PTP_ChatAct";
+	
+	ChatFragment mChatFrag = null;
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		try{
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.chat);
+			Intent i = getIntent();
+			String initMsg = i.getStringExtra("FIRST_MSG");
+			
+			initFragment(initMsg);
+		}
+		catch(Exception e) { Toast.makeText(this, "On Create Failed",  Toast.LENGTH_SHORT).show(); }
+	}
+	
+	/**
+	 * init fragement with possible recvd start up message.
+	 */
+	public void initFragment(String initMsg) {
+		try{
+	    	// to add fragments to your activity layout, just specify which viewgroup to place the fragment.
+	    	final FragmentTransaction ft = getFragmentManager().beginTransaction();
+	    	if( mChatFrag == null ){
+	    		//mChatFrag = ChatFragment.newInstance(this, ConnectionService.getInstance().mConnMan.mServerAddr);
+	    		mChatFrag = ChatFragment.newInstance(this, null, initMsg);
+	    	}
+	    	
+	    	Log.d(TAG, "initFragment : show chat fragment..." + initMsg);
+	    	// chat fragment on top, do not do replace, as frag_detail already hard coded in layout.
+	    	ft.add(R.id.frag_chat, mChatFrag, "chat_frag");
+	    	ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+	    	ft.commit();
+		}
+		catch(Exception e) { Toast.makeText(this, "Init fragment Failed",  Toast.LENGTH_SHORT).show(); }
+    }
+	
+	@Override
+	public void onResume() {
+		try{
+	        super.onResume();
+	        Log.d(TAG, "onResume: chat activity resume, register activity to connection service !");
+	        registerActivityToService(true);
+		}
+		catch(Exception e) { Toast.makeText(this, "On Resume Failed",  Toast.LENGTH_SHORT).show(); }
+    }
+	
+	@Override
+	public void onPause() {
+		try{
+			super.onPause();
+			Log.d(TAG, "onPause: chat activity closed, de-register activity from connection service !");
+			registerActivityToService(false);
+		}
+		catch(Exception e) { Toast.makeText(this, "On Pause Failed",  Toast.LENGTH_SHORT).show(); }
+	}
+
+	@Override
+	public void onDestroy() {
+		try{
+			super.onDestroy();
+			Log.d(TAG, " onDestroy: disconnect p2p connection. ");
+		}
+		catch(Exception e) { Toast.makeText(this, "On Destroy Failed",  Toast.LENGTH_SHORT).show(); }
+	}
+	
+	/**
+	 * set listview weight works, only when layout has only one LinearLayout.
+	 */
+	@Deprecated
+	private void testWithListViewWeight() {
+		try{
+			List<String> mMessageList;   // a list of chat msgs.
+		    ArrayAdapter<String> mAdapter;
+		    
+			mMessageList = new ArrayList<String>(200);
+			for(int i=0;i<100;i++)
+				mMessageList.add("User logged in");
+	        mAdapter = new ChatMessageAdapter(this, mMessageList);
+	        
+	        //setListAdapter(mAdapter);  // list fragment data adapter
+	        mAdapter.notifyDataSetChanged();  // notify the attached observer and views to refresh.
+	        Toast.makeText(this, "User logged in",  Toast.LENGTH_SHORT).show();
+		}
+		catch(Exception e) { Toast.makeText(this, "List View Failed",  Toast.LENGTH_SHORT).show(); }
+	}
+	
+	
+    /**
+     * chat message adapter from list adapter.
+     * Responsible for how to show data to list fragment list view.
+     */
+    final class ChatMessageAdapter extends ArrayAdapter<String> {
+
+    	private LayoutInflater mInflater;
+    	
+		public ChatMessageAdapter(Context context, List<String> objects){
+			super(context, 0, objects);
+            mInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+		
+		@Override
+        public int getItemViewType(int position) {
+			return IGNORE_ITEM_VIEW_TYPE;   // do not care			
+		}
+		
+		/**
+		 * assemble each row view in the list view.
+		 */
+		@Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+			View view = convertView;  // old view to re-use if possible. Useful for Heterogeneous list with diff item view type.
+			try{
+	            String item = this.getItem(position);
+	            
+	            if( view == null ){
+	            	view = mInflater.inflate(R.layout.msg_row, null);
+	            }
+	            
+	            TextView msgRow = (TextView)view.findViewById(R.id.msg_row);
+	            msgRow.setText(item);
+	            Toast.makeText(this.getContext(), item + " received",  Toast.LENGTH_SHORT).show();
+			}
+			catch(Exception e) { Toast.makeText(this.getContext(), "Get View Failed",  Toast.LENGTH_SHORT).show(); }
+            return view;
+		}
+    }
+    
+    /**
+     * register this activity to service process, so that service later can update list view.
+     * In AsyncTask, the activity itself is passed to the constructor of asyncTask, hence later
+     * onPostExecute() can do mActivity.mCommentsAdapter.notifyDataSetChanged();
+     * Just need to be careful of reference to avoid mem leak. 
+     */
+    protected void registerActivityToService(boolean register){
+    	try{
+	    	if( ConnectionService.getInstance() != null ){
+		    	Message msg = ConnectionService.getInstance().getHandler().obtainMessage();
+		    	msg.what = MSG_REGISTER_ACTIVITY;
+		    	msg.obj = this;
+		    	msg.arg1 = register ? 1 : 0;
+		    	ConnectionService.getInstance().getHandler().sendMessage(msg);
+	    	}
+    	}
+    	catch(Exception e) { Toast.makeText(this, "Register Activity Failed",  Toast.LENGTH_SHORT).show(); }
+    }
+    
+    /**
+     * post send msg to service to handle it in background.
+     */
+    public void pushOutMessage(String jsonstring) {
+    	try{
+	    	Log.d(TAG, "pushOutMessage : " + jsonstring);
+	    	Message msg = ConnectionService.getInstance().getHandler().obtainMessage();
+	    	msg.what = MSG_PUSHOUT_DATA;
+	    	msg.obj = jsonstring;
+	    	ConnectionService.getInstance().getHandler().sendMessage(msg);
+    	}
+    	catch(Exception e) { Toast.makeText(this, "Push Out Msg Failed",  Toast.LENGTH_SHORT).show(); }
+    }
+    
+    /**
+     * show the msg in chat fragment
+     */
+    public void showMessage(String msg){
+    	try{
+	    	MsgRow row = ChatFragment.MsgRow.parseMsgRow(msg);
+	    	Log.d(TAG, "showMessage : " + msg + " : " + row.mMsg);
+	    	if( mChatFrag != null ){
+	    		mChatFrag.appendChatMessage(row);
+	    	}
+    	}
+    	catch(Exception e) { Toast.makeText(this, "Show Msg Failed",  Toast.LENGTH_SHORT).show(); }
+    }
+}
