10e11805c1578329c501e794595af9ee626dfb7d
Qilin Wang <wangqilin129@gmail.com>
2016-03-11 09:43:12 +0800

    disable the reconnection feature after disconnect

--- app/src/main/java/com/colorcloud/wifichat/WiFiDirectBroadcastReceiver.java
+++ app/src/main/java/com/colorcloud/wifichat/WiFiDirectBroadcastReceiver.java
@@ -114,48 +114,41 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
                             //while(!networkInfo.isConnected()){
                             try {
                                 manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
-
                                     @Override
                                     public void onSuccess() {
-                                        //Toast.makeText(activity, "Discovery Initiated", Toast.LENGTH_SHORT).show();
-//                                        Log.d(TAG, "Peer Discovery Success");
                                     }
 
                                     @Override
                                     public void onFailure(int reasonCode) {
-                                        //Toast.makeText(activity, "Discovery Failed : " + reasonCode, Toast.LENGTH_SHORT).show();
-//                                        Log.d(TAG, "Peer Discovery Failed");
                                     }
                                 });
+
                                 manager.requestPeers(channel, (PeerListListener) activity.getFragmentManager()
                                         .findFragmentById(R.id.frag_list));
-                                WifiP2pConfig config = new WifiP2pConfig();
-                                config.deviceAddress = WiFiDirectActivity.partnerDevice;
-                                config.wps.setup = WpsInfo.PBC;
-//                                Log.d(TAG, "Before Connecting to partner");
-                                activity.connect(config);
-//                                Log.d(TAG, "After Connecting to partner");
+
+//                                WifiP2pConfig config = new WifiP2pConfig();
+//                                config.deviceAddress = WiFiDirectActivity.partnerDevice;
+//                                config.wps.setup = WpsInfo.PBC;
+//                                activity.connect(config);
+
                                 if (networkInfo.isConnectedOrConnecting()) {
                                     Handler handler = new Handler();
                                     handler.postDelayed(new Runnable() {
                                         public void run() {
-//                                            Log.d(TAG, "Wait Done 3");
                                         }
                                     }, 2000);
                                 }
                             } catch (Exception e) {
-//                                Log.d(TAG, "2 error: " + e.toString());
+                                Log.e(TAG, "* error: " + e.toString());
                                 //r++;
                                 //if(r >= rcap) break;
                                 Handler handler = new Handler();
                                 handler.postDelayed(new Runnable() {
                                     public void run() {
-//                                        Log.d(TAG, "Wait Done 4");
                                     }
                                 }, 500);
                             }
                         }
-                        //}
                     }
                 }
 //                Log.d(TAG, "WIFI_P2P_CONNECTION_CHANGED_ACTION = " + networkInfo.describeContents());
@@ -176,17 +169,12 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
                     WifiManager wifi = (WifiManager) activity.getSystemService(Context.WIFI_SERVICE);
                     wifi.setWifiEnabled(true);
                     manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
-
                         @Override
                         public void onSuccess() {
-                            //Toast.makeText(activity, "Discovery Initiated", Toast.LENGTH_SHORT).show();
-//                            Log.d(TAG, "Peer Discovery Success");
                         }
 
                         @Override
                         public void onFailure(int reasonCode) {
-                            //Toast.makeText(activity, "Discovery Failed : " + reasonCode, Toast.LENGTH_SHORT).show();
-//                            Log.d(TAG, "Peer Discovery Failed");
                         }
                     });
                     manager.requestPeers(channel, (PeerListListener) activity.getFragmentManager()
@@ -196,11 +184,11 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
                     config.deviceAddress = WiFiDirectActivity.partnerDevice;
                     config.wps.setup = WpsInfo.PBC;
                     activity.connect(config);
+
                     if (networkInfo.isConnectedOrConnecting()) {
                         Handler handler = new Handler();
                         handler.postDelayed(new Runnable() {
                             public void run() {
-//                                Log.d(TAG, "Wait Done 3");
                             }
                         }, 2000);
                     }
@@ -209,7 +197,6 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
                     Handler handler = new Handler();
                     handler.postDelayed(new Runnable() {
                         public void run() {
-//                            Log.d(TAG, "Wait Done 4");
                         }
                     }, 500);
                 }
